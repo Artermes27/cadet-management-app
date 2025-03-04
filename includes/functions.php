a9bc84380ee27a2e076192c0da31cef7dbd207b6
= <=>
2025-03-04 17:34:56 +0000

    misalanious

--- includes/functions.php
+++ includes/functions.php
@@ -3,22 +3,30 @@
 function check_login($con){
 		if(isset($_SESSION['user_id']) && isset($_SESSION['password'])){
 				$id = $_SESSION['user_id'];
-		$password = $_SESSION['password'];
+				$password = $_SESSION['password'];
 				$query = "SELECT * FROM users Where `user_id` = '$id' AND `password` = '$password' limit 1;";
-		$result = mysqli_query($con,$query);
+				$result = mysqli_query($con,$query);
 				if($result && mysqli_num_rows($result) > 0){
-			$user_data = mysqli_fetch_assoc($result);
-						if (isset($_GET["parade_id"])){
-				$user_data["parade_id"] = $_GET["parade_id"];
-			}if (isset($_GET["event_id"])){
-				$user_data["event_id"] = $_GET["event_id"];
-			}
-			return $user_data;
+				$user_data = mysqli_fetch_assoc($result);
+				if (isset($_GET["parade_id"])){
+					$user_data["parade_id"] = $_GET["parade_id"];
+				}if (isset($_GET["event_id"])){
+					$user_data["event_id"] = $_GET["event_id"];
+				}
+				return $user_data;
 		}
 	}
 		header("Location: login.php");
 	die;
 
 }
+/*
+function check_login($con){
+	$query = "SELECT * FROM users WHERE user_id = 0;";
+	$result = mysqli_query($con, $query);
+	$user_data = mysqli_fetch_assoc($result);
+	return $user_data;
+}
+*/
 
 ?>
--- sandbox.php
+++ sandbox.php
@@ -3,5 +3,5 @@
 	include_once("includes/connection.php");
 	include("includes/functions.php");
 	$user_data = check_login($con);
-    include("includes/nav.php"); 
+    include("includes/nav.php");
 ?>
