662d4ad82f48bb28328f4c18c42d54be20f029fc
= <=>
2025-03-29 11:39:30 +0000

    updated uploads to ensure corect redirection an error message prompting

--- uploads.php
+++ uploads.php
@@ -1,4 +1,4 @@
-<a href="profile.php">click here to go back to the profile page</a><br>
+<a href="calendar.php">click here to go back to the calendar page</a><br>
 <?php 
 session_start();
 	include_once("includes/connection.php");
@@ -9,6 +9,7 @@ $target_dir = "profile-photos/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$error_msg = "";
 
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -20,12 +21,11 @@ if(isset($_POST["submit"])) {
 }
 
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    ?>your image is to large try croping it</br><?php
+    $error_msg .= "<a>your image is to large try croping it</a><br>";
     $uploadOk = 0;
 }
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    ?>your image was not one of the following alowed types: jpg, jpeg or png</br><?php
-    die;
+    $error_msg = "<a>your image was not one of the following alowed types: jpg, jpeg or png</a><br>";
     $uploadOk = 0;
 }
 
@@ -48,14 +48,14 @@ $target_file = $target_dir . $user_data["user_id"] . "." . $imageFileType;
 
 
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo "<a>Sorry, your file was not uploaded.</a><br>";
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
     header("Location: calendar.php");
     die;
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo $error_msg;
     die;
   }
 }
