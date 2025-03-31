fa33ba4fad5b0fe4ea7375d7a513af08982d5701
= <=>
2025-03-31 14:56:16 +0100

    incresed the functionalisation of the calendar page

--- calendar.php
+++ calendar.php
@@ -6,6 +6,7 @@ session_start();
 
   $user_data = check_login($con);
 
+  //function to get the dates the skip forwards/backwards buttons should redirect the user to
   function date_skip_method($con, $current_date, $skip){
     if($skip == "-1"){
       $query = "SELECT date FROM parades WHERE date < '$current_date' ORDER BY date DESC limit 1;";
@@ -72,16 +73,17 @@ session_start();
     }
   }
 
+  //function to return an array of 5 dates and the final valid date in the array. 
   function get_parade_date_range($con, $current_date, $admin)	{
     $query = "SELECT date FROM parades WHERE date >= '$current_date' ORDER BY date ASC limit 5;";
     $result = mysqli_query($con, $query);
     $dates = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    if (count($dates) < 5)  {
+    if (count($dates) < 5)  {//if their are less than 5 dates then the array is filled with an error message
       $end_date = $dates[count($dates)-1]["date"];
       while(count($dates) < 5) {
-        $dates[] = ["date" => "no parades beyond this date"];
+        $dates[] = ["date" => "null"];
       }
-    } else {
+    } else {//the admin will have one less date displayed since they have the admin edit panel displayed
       if($admin == 1){        
         $end_date = $dates[count($dates)-2]["date"];
       }else{
@@ -91,79 +93,112 @@ session_start();
     return [$dates, $end_date];
   }
 
-  function html_for_parade_on_callendar($con, $parade_date, $user_data){    
+  function html_for_displaying_an_event($con, $event_id, $event_display_type, $user_id){
+    $query = "SELECT * FROM events WHERE events.event_id = " . $event_id . ";";
+    $result = mysqli_query($con, $query);
+    $event = mysqli_fetch_assoc($result);
+    //desciding the colour of the event based off its aproval status
+    if($event["final_aproval"] == 0){
+      $style_class = "event_not_aproved";
+    }elseif($event["final_aproval"] == 1){
+      $style_class = "event_aproved";
+    }elseif($event["final_aproval"] == 2){
+      $style_class = "event_aproval_requested";
+    }
+    if($event_display_type == "admin"){//if the user is an admin then display the event owners full name and the edit on admin panel edit button
+      $query = "SELECT `first_name`, `last_name`, `rank` FROM users WHERE user_id = " . $event["owner"] . ";";
+      $result = mysqli_query($con, $query);
+      $owner = mysqli_fetch_assoc($result);
+      $return_html = "<div class=\"" . $style_class . "\"><a>" . $event["event_start"] .  " till " . $event["event_end"] . "</a><br>\n<a>" . $event["event_name"] . "</a><br>\n<a>" . $owner["rank"] . " " . $owner["first_name"] . " " . $owner["last_name"] . "</a><br>\n<a href=\"event.php?parade_id=" . $event["parade_id"] . "&event_id=" . $event["event_id"] . "\">" .  $event["event_name"] . "</a><br>\n<button onclick=\"populateAdminEditEventForm('" . $event["parade_id"] . "', '" . $event["event_id"] . "', '" . $event["event_type"] . "', '" . $event["event_name"] . "', '" .  $event["event_start"] . "', '" . $event["event_end"] . "', '" . $event["owner"] . "', '" .  $event["final_aproval"] . "')\">click for admin panel edit</button></div>\n";
+    }elseif($event_display_type == "G4"){//if in future events were to be displayed differently they can be easilly changed here
+      $return_html = "<div class=\"" . $style_class . "\"><a>" . $event["event_start"] .  " till " . $event["event_end"] . "</a><br>\n<a href=\"event.php?parade_id=" . $event["parade_id"] . "&event_id=" . $event["event_id"] . "\">" .  $event["event_name"] . "</a></div>\n";
+    }elseif($event_display_type == "standard"){
+      if ($event["owner"] == $user_id){
+        $return_html = "<div class=\"" . $style_class . "\"><a>" . $event["event_start"] .  " till " . $event["event_end"] . "</a><br>\n<a href=\"event.php?parade_id=" . $event["parade_id"] . "&event_id=" . $event["event_id"] . "\">" .  $event["event_name"] . "</a></div>\n";
+      }elseif($event["duty"] == $user_id){
+        $return_html = "<div class=\"" . $style_class . "\"><a>duty event</a><br><a>" . $event["event_start"] .  " till " . $event["event_end"] . "</a><br>\n<a href=\"event.php?parade_id=" . $event["parade_id"] . "&event_id=" . $event["event_id"] . "\">" .  $event["event_name"] . "</a></div>\n";
+      }else{
+        $return_html = "<div class=\"event\"><a>" . $event["event_start"] .  " till " . $event["event_end"] . "</a><br>\n<a>" . $event["event_name"] . "</a></div>\n";
+      }
+    }
+    return $return_html;
+  }
+
+  function html_for_equipment_request_log($equipment_reuqest_log){//function to display and equipment request log given an array of equipment requests
+    $log = "<div class=\"event\">\n";
+    $log .= "<h3 style=\"text-decoration: underline;\">log of aproved equipment requests</h3>";
+    $log .= "<table>";
+    $log .= "<tr>\n";
+    $log .= "<th>name</th>";
+    $log .= "<th>location</th>";
+    $log .= "<th>start time</th>";
+    $log .= "<th>finish time</th>";
+    $log .= "<th>lesson name</th>";
+    $log .= "</tr>";
+    foreach ($equipment_reuqest_log as $request) { 
+      $log .= "<tr>";
+      $log .= "<td>" . $request["name"] . "</td>";
+      $log .= "<td>" . $request["location"] . "</td>";
+      $log .= "<td>" . $request["event_start"] . "</td>";
+      $log .= "<td>" . $request["event_end"] . "</td>";
+      $log .= "<td>" . $request["event_name"] . "</td>";
+      $log .= "</tr>";
+    }
+    $log .= "</table>";
+    $log .= "</div>\n";
+    return $log;
+  }
+  
+  //fuction to generate all the html for a parade with a known date
+  function html_for_parade_on_callendar($con, $parade_date, $user_data){
+    //selecting the parade_id
     $query = "SELECT parade_id, parade_name FROM parades WHERE date = '$parade_date';";
     $result = mysqli_query($con, $query);
     $parade = mysqli_fetch_assoc($result);
-    if($user_data["admin"] == 1 or $user_data["G4"] == 1){      
+    //setting the display type of the user i.e. should they see an equipment request log or should they have the admin panel edit button
+    if($user_data["admin"] == 1 or $user_data["G4"] == 1){//if the user is admin or G4 then display all events for the parade
       $query = "SELECT events.* FROM events WHERE parade_id =" . $parade["parade_id"] . " ORDER BY events.event_start;";
-    }else{
+      $event_display_type = "admin";
+    }elseif($user_data["G4"] == 1){
+      $query = "SELECT events.* FROM events WHERE parade_id =" . $parade["parade_id"] . " ORDER BY events.event_start;";
+      $event_display_type = "G4";
+    }else{//if the user is not admin or G4 then they should only see events they are related to
       $query = "SELECT DISTINCT events.* FROM events LEFT JOIN user_event ON events.event_id = user_event.event_id WHERE (parade_id = " . $parade["parade_id"] . " AND user_event.user_id = " . $user_data["user_id"] . ") OR ((events.duty = " . $user_data["user_id"] . " OR events.owner = " . $user_data["user_id"] . ") AND parade_id = " . $parade["parade_id"] . ") ORDER BY events.event_start;";
+      $event_display_type = "standard";
     }
     $result = mysqli_query($con, $query);
     $events = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $parade_html = "<div class=\"event\"><h2>" . $parade_date . "</h2><h2>" . $parade["parade_name"] . "</h2>";
+    $parade_html .= "</div>\n";
     $event_count = 0;
-    $event_html = "<div class=\"event\"><h2>" . $parade_date . "</h2><h2>" . $parade["parade_name"] . "</h2>";
-    $event_html .= "</div>\n";
-    if(count($events) == 0){      
-      $event_html = $event_html . "<div class=\"event\"><a>you have no events on this parade night</a></div>";
-    }else{      
-      $equipment_reuqest_log = [];
-      while($event_count < count($events)){
-        if($events[$event_count]["final_aproval"] == 0){
-          $style_class = "event_not_aproved";
-        }elseif($events[$event_count]["final_aproval"] == 1){
-          $style_class = "event_aproved";
-        }elseif($events[$event_count]["final_aproval"] == 2){
-          $style_class = "event_aproval_requested";
-        }
-        if($user_data["admin"] == 1){
-          $query = "SELECT `first_name`, `last_name`, `rank` FROM users WHERE user_id = " . $events[$event_count]["owner"] . ";";
+    if(count($events) == 0){
+      $parade_html .= "<div class=\"event\"><a>you have no events on this parade night</a></div>";
+    }else{
+      if ($user_data["G4"] == 1){//if the user is G4 then an equipment request log must be produced
+        while($event_count < count($events)){//loop to produce the html for each event and append the equipment requests to the array of requests
+          $parade_html .= html_for_displaying_an_event($con, $events[$event_count]["event_id"], $event_display_type, $user_data["user_id"]);
+          //query to selct approved equipment requests for an event which are appended to the array equipment_request_log
+          $query = "SELECT equipment.name, equipment.location, events.event_name, events.event_start, events.event_end, users.rank, users.first_name, users.last_name FROM equipment, events, users, equipment_requests WHERE events.event_id = " . $events[$event_count]["event_id"] . " AND users.user_id = events.owner AND equipment_requests.event_id = events.event_id AND equipment.equipment_id = equipment_requests.equipment_id AND equipment_requests.aproved = 1";
           $result = mysqli_query($con, $query);
-          $owner = mysqli_fetch_assoc($result);
-          $event_html = $event_html . "<div class=\"" . $style_class . "\"><a>" . $events[$event_count]["event_start"] .  " till " . $events[$event_count]["event_end"] . "</a><br>\n<a>" . $events[$event_count]["event_name"] . "</a><br>\n<a>" . $owner["rank"] . " " . $owner["first_name"] . " " . $owner["last_name"] . "</a><br>\n<a href=\"event.php?parade_id=" . $parade["parade_id"] . "&event_id=" . $events[$event_count]["event_id"] . "\">" .  $events[$event_count]["event_name"] . "</a><br>\n<button onclick=\"populateAdminEditEventForm('" . $events[$event_count]["parade_id"] . "', '" . $events[$event_count]["event_id"] . "', '" . $events[$event_count]["event_type"] . "', '" . $events[$event_count]["event_name"] . "', '" .  $events[$event_count]["event_start"] . "', '" . $events[$event_count]["event_end"] . "', '" . $events[$event_count]["owner"] . "', '" .  $events[$event_count]["final_aproval"] . "')\">click for admin panel edit</button></div>\n";
-        }elseif($user_data["user_id"] == $events[$event_count]["owner"] or $user_data["G4"] == 1){
-          $event_html = $event_html . "<div class=\"" . $style_class . "\"><a>" . $events[$event_count]["event_start"] .  " till " . $events[$event_count]["event_end"] . "</a><br>\n<a href=\"event.php?parade_id=" . $parade["parade_id"] . "&event_id=" . $events[$event_count]["event_id"] . "\">" .  $events[$event_count]["event_name"] . "</a></div>\n";
-        }elseif($user_data["user_id"] == $events[$event_count]["duty"]){
-          $event_html = $event_html . "<div class=\"" . $style_class . "\"><a>duty event</a><br><a>" . $events[$event_count]["event_start"] .  " till " . $events[$event_count]["event_end"] . "</a><br>\n<a href=\"event.php?parade_id=" . $parade["parade_id"] . "&event_id=" . $events[$event_count]["event_id"] . "\">" .  $events[$event_count]["event_name"] . "</a></div>\n";
-        }else {
-          $event_html = $event_html . "<div class=\"event\"><a>" . $events[$event_count]["event_start"] .  " till " . $events[$event_count]["event_end"] . "</a><br>\n<a>" . $events[$event_count]["event_name"] . "</a></div>\n";
+          while($request = mysqli_fetch_assoc($result)){//appending to the equipment request log
+            $equipment_reuqest_log[] = $request;
+          }
+          $event_count = $event_count + 1;
         }
-        $query = "SELECT equipment.name, equipment.location, events.event_name, events.event_start, events.event_end, users.rank, users.first_name, users.last_name FROM equipment, events, users, equipment_requests WHERE events.event_id = " . $events[$event_count]["event_id"] . " AND users.user_id = events.owner AND equipment_requests.event_id = events.event_id AND equipment.equipment_id = equipment_requests.equipment_id AND equipment_requests.aproved = 1";
-        $result = mysqli_query($con, $query);
-        while($request = mysqli_fetch_assoc($result)){
-          $equipment_reuqest_log[] = $request;
+        if (count($equipment_reuqest_log) > 0){//outputing an equipment request log if their are requests
+          $parade_html .= html_for_equipment_request_log($equipment_reuqest_log);
         }
-        $event_count = $event_count + 1;
-      }
-      if($user_data["G4"] == "1" and count($equipment_reuqest_log) > 0){
-        $event_html .= "<div class=\"event\">\n";
-        $event_html .= "<h3 style=\"text-decoration: underline;\">log of aproved equipment requests</h3>";
-        $event_html .= "<table>";
-        $event_html .= "<tr>\n";
-        $event_html .= "<th>name</th>";
-        $event_html .= "<th>location</th>";
-        $event_html .= "<th>start time</th>";
-        $event_html .= "<th>finish time</th>";
-        $event_html .= "<th>lesson name</th>";
-        $event_html .= "</tr>";
-        foreach ($equipment_reuqest_log as $request) { 
-          $event_html .= "<tr>";
-          $event_html .= "<td>" . $request["name"] . "</td>";
-          $event_html .= "<td>" . $request["location"] . "</td>";
-          $event_html .= "<td>" . $request["event_start"] . "</td>";
-          $event_html .= "<td>" . $request["event_end"] . "</td>";
-          $event_html .= "<td>" . $request["event_name"] . "</td>";
-          $event_html .= "</tr>";
+      }else{
+        while($event_count < count($events)){//loop to produce the html for each event
+          $parade_html .= html_for_displaying_an_event($con, $events[$event_count]["event_id"], $event_display_type, $user_data["user_id"]);
+          $event_count = $event_count + 1;
         }
-        $event_html .= "</table>";
-        $event_html .= "</div>\n";
       }
     }
-    return $event_html;
+  return $parade_html;
   }
 
-  function html_for_admin_page_on_callandar(){
+  function html_for_admin_page_on_callandar(){//function to output the html needed for the admin panel edit form 
     $html = "<div class=\"event\">";
     $html .= "<h2>admin panel</h2>\n";
     $html .= "<link rel=\"stylesheet\" href=\"css/event-owner-form-style.css\">\n";
@@ -204,21 +239,25 @@ session_start();
     $html .= "</div>\n";
     return $html;
   }
+
   include("requests/get_request_scanning.php");
+  //retreving the starting date for the calendar page
   if(isset($_GET["current_date"]) and get_request("current_date") != "null") {
-    $current_date = $_GET["current_date"];
-  } else {
+    $current_date = get_request("current_date");
+  } else {//no starting date set so use todays date
     $current_date = str_replace("/", "-", date("Y/m/d"));
   }
+  //getting the parade date range and the end date storing in variable temp because functions can only output one object
   $temp = get_parade_date_range($con, $current_date, $user_data["admin"]);
   $parade_dates = $temp[0];
   $end_date = $temp[1];
+  $temp = null;
   $min_add = date_skip_method($con, $current_date, "+1");
   $min_subtract = date_skip_method($con, $current_date, "-1");
-  if($user_data["admin"] == 0) {
+  if($user_data["admin"] == 0) {//if the user doesent have the admin edit panel then they use all 5 slots on the calendar page so they skip forward/back by 5 parades
     $max_add = date_skip_method($con, $current_date, "+5");
     $max_subtract = date_skip_method($con, $current_date, "-5");
-  } else {
+  } else {//admins have the admin panel edit so they only skip by 4 parades
     $max_add = date_skip_method($con, $current_date, "+4");
     $max_subtract = date_skip_method($con, $current_date, "-4");
   }
@@ -263,16 +302,44 @@ session_start();
         ?>
       </div>
       <div class="parade2">
-        <?php echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data)); $output_count = $output_count + 1;?>
+        <?php 
+          if ($parade_dates[$output_count]["date"] == "null"){
+            echo("<div class=\"event\"><h2>no parades beyond this point</h2></div>");
+          }else{
+            echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data));
+          }
+          $output_count = $output_count + 1;
+        ?>
       </div>
       <div class="parade3">
-        <?php echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data)); $output_count = $output_count + 1;?>
+        <?php 
+          if ($parade_dates[$output_count]["date"] == "null"){
+            echo("<div class=\"event\"><h2>no parades beyond this point</h2></div>");
+          }else{
+            echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data));
+          }
+          $output_count = $output_count + 1;
+        ?>
       </div>
       <div class="parade4">
-        <?php echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data)); $output_count = $output_count + 1;?>
+        <?php 
+          if ($parade_dates[$output_count]["date"] == "null"){
+            echo("<div class=\"event\"><h2>no parades beyond this point</h2></div>");
+          }else{
+            echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data));
+          }
+          $output_count = $output_count + 1;
+        ?>
       </div>
       <div class="parade5">
-        <?php echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data)); $output_count = $output_count + 1;?>
+        <?php 
+          if ($parade_dates[$output_count]["date"] == "null"){
+            echo("<div class=\"event\"><h2>no parades beyond this point</h2></div>");
+          }else{
+            echo(html_for_parade_on_callendar($con, $parade_dates[$output_count]["date"], $user_data));
+          }
+          $output_count = $output_count + 1;
+        ?>
       </div>
     </div>
   </div>
